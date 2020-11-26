7afd78d2abc0716826f807962a851bbd254bdbc6
Данил Цесаренко <danil-master@outlook.com>
2020-11-26 23:01:54 +0200

    Added class Logger

---
+++ Logger/Logger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public class Starter
+    {
+        public void Run()
+        {
+            var rand = new Random();
+            var action = new Actions();
+            var log = Logger.GetInstance();
+
+            for (int i = 0; i < 100; i++)
+            {
+                try
+                {
+                    int funcRandNum = rand.Next(1, 4);
+                    switch (funcRandNum)
+                    {
+                        case 1:
+                            action.Info();
+                            break;
+                        case 2:
+                            action.Warning();
+                            break;
+                        case 3:
+                            action.Error();
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(log.NewMessage("Error", ex));
+                }
+            }
+        }
+
+    }
+}
