7e228dbecde702d18ba01703399202eb582a39fa
Данил Цесаренко <danil-master@outlook.com>
2020-11-26 23:07:13 +0200

    Added method call Run

--- Logger/Program.cs
+++ Logger/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Logger
 {
@@ -10,7 +11,8 @@ namespace Logger
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Starter Runner = new Starter();
+            Runner.Run();
         }
     }
 }
